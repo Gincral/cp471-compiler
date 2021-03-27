c838b4974a43a749d793a723f4adcc924dfd358c
Gincral <ssssrxdd@gmail.com>
2021-03-27 15:27:58 -0400

    symbol table

---
+++ src/symbol.py
@@ -0,0 +1,28 @@
+
+
+class SymbolTable:
+    def __int__(self):
+        self.dataList = []
+
+    def appendData(self, data):
+        self.append(data)
+
+    def searchData(self, name):
+        for data in self.dataList:
+            if data.name == name: return data
+        return None
+    
+    def updateDataValue(self, name, value):
+        for data in self.dataList:
+            if data.name == name: 
+                data.value = value
+                return data
+        return None
+    
+
+class Data:
+    def __int__(self, name, type, value, location):
+        self.name = name
+        self.type = type
+        self.value = value
+        self.location = location
