6cf96b9e1e05cdd47f7d5f40b61490a24061dfea
Gincral <ssssrxdd@gmail.com>
2021-03-27 15:08:17 -0400

    file input abd output

--- src/main.py
+++ src/main.py
@@ -1,18 +1,27 @@
 import sys
-from .lexer import *
-from .inter import *
-from .parser import *
-from .symbol import *
+# from .lexer import *
+# from .inter import *
+# from .parser import *
+# from .symbol import *
 
 
 def main():
-    print("haha")
-	# input_file_address= sys.argv[1]
-    # output_file_address= sys.argv[2]
+    input_file_address= sys.argv[1]
+    output_file_address= sys.argv[2]
+    input_file_address = "input/input.txt"
+    output_file_address = "output/result.txt"
+
+    input_file = open(input_file_address, "r")
+    output_file = open(output_file_address, "w")
+
+    input_file.close()
+    output_file.close()
+    
 	# lex = new lex()
 	# symbol = new symbol()
 	# parser = new parser(lex, symbol, input_file_address, output_file_address)
 	# parser.start()
+
 
 if __name__ == "__main__":
     main()
