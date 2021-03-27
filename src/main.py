ba6b97a2934dd924f75eacff97f61c0bc766c0f9
Nina Yang <ssssrxdd@gmail.com>
2021-03-27 14:39:17 -0400

    set up

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import sys
+from .lexer import *
+from .inter import *
+from .parser import *
+from .symbol import *
+
+
+def main():
+    print("haha")
+	# input_file_address= sys.argv[1]
+    # output_file_address= sys.argv[2]
+	# lex = new lex()
+	# symbol = new symbol()
+	# parser = new parser(lex, symbol, input_file_address, output_file_address)
+	# parser.start()
+
+if __name__ == "__main__":
+    main()
