f2fd3640e0bacb8b6bd4373f154d8b9d5eaeffae
Gincral <ssssrxdd@gmail.com>
2021-03-31 18:42:26 -0400

    parser

--- src/parser.py
+++ src/parser.py
@@ -7,28 +7,28 @@ class Node:
 
 def parser (symbolTable, tokenList):
     if (tokenList[len(tokenList)-1].value == ';'): tokenList = tokenList[:len(tokenList)-1]
-    root = Node(tokenList)
-    stmt(root)
+    stmtN = Node(tokenList)
+    stmt(stmtN)
     return None
 
 def stmt(root):
     print("Grammar stmt")
     tokenList = root.value
     if tokenList[0].type == 'id':
-        node = Node(tokenList)
-        root.children.append(node)
-        return assign(node)
+        assignN = Node(tokenList)
+        root.children.append(assignN)
+        return assign(assignN)
     else:
         if tokenList[0].value == 'if':
             return "haha"
         elif tokenList[0].value == 'print':
-            node = Node(tokenList)
-            root.children.append(node)
-            return println(node)
+            printN = Node(tokenList)
+            root.children.append(printN)
+            return println(printN)
         elif tokenList[0].value == 'def':
-            node = Node(tokenList)
-            root.children.append(node)
-            return dec(node)
+            decN = Node(tokenList)
+            root.children.append(decN)
+            return dec(decN)
         else:
             print("wrong grammar: stmt")
             return ValueError
@@ -43,13 +43,13 @@ def println(root):
     tokenList = root.value
     le = len(tokenList)
     if tokenList[1].value == '(' and tokenList[le-1].value == ')':
-        node = Node(tokenList[2:le-2])
+        boolN = Node(tokenList[2:le-2])
         root.children.append(Node(tokenList[0]))
         root.children.append(Node(tokenList[1]))
-        root.children.append(node)
+        root.children.append(boolN)
         root.children.append(Node(tokenList[le-2]))
         root.children.append(Node(tokenList[le-1]))
-        return bool(node)
+        return bool(boolN)
     else:
         print("wrong grammar: println")
         return ValueError
@@ -59,13 +59,13 @@ def dec(root):
     tokenList = root.value
     le = len(tokenList)
     if tokenList[1].type == 'lex' and tokenList[2].type == 'id' and tokenList[3].value == '=':
-        node = Node(tokenList[2:le-2])
+        boolN = Node(tokenList[2:le-2])
         root.children.append(Node(tokenList[0]))
         root.children.append(Node(tokenList[1]))
-        root.children.append(node)
+        root.children.append(boolN)
         root.children.append(Node(tokenList[le-2]))
         root.children.append(Node(tokenList[le-1]))
-        return bool(node)
+        return bool(boolN)
     else:
         print("wrong grammar: println")
         return ValueError
@@ -74,55 +74,171 @@ def assign(root):
     print("Grammar assign")
     tokenList = root.value
     if tokenList[1].value == '=':
-        node = Node(tokenList[2:])
+        boolN = Node(tokenList[2:])
         root.children.append(Node(tokenList[0]))
         root.children.append(Node(tokenList[1]))
-        root.children.append(node)
-        return bool(node)
+        root.children.append(boolN)
+        return bool(boolN)
     else:
         print("wrong grammar: assign")
         return ValueError
 
 def bool(root):
     print("Grammar bool")
-    return None
+    tokenList = root.value
+    for i in range(len(tokenList)):
+        if tokenList[i].value == '||' or tokenList[i].value == '&&':
+            equalityN = Node(tokenList[:i])
+            bool1N =  Node(tokenList[i:])
+            root.children.append(Node(equalityN))
+            root.children.append(Node(bool1N))
+            return equality(equalityN) and bool1(bool1N)
+    equalityN = Node(tokenList)
+    root.children.append(Node(equalityN))
+    return equality(equalityN)
 
 def bool1(root):
     print("Grammar bool1")
-    return None
+    tokenList = root.value
+    root.children.append(Node(tokenList[0]))
+    tokenList = tokenList[1:]
+    for i in range(len(tokenList)):
+        if tokenList[i].value == '||' or tokenList[i].value == '&&':
+            equalityN = Node(tokenList[:i])
+            bool1N =  Node(tokenList[i:])
+            root.children.append(Node(equalityN))
+            root.children.append(Node(bool1N))
+            return equality(equalityN) and bool1(bool1N)
+    equalityN = Node(tokenList)
+    root.children.append(Node(equalityN))
+    return equality(equalityN)
 
 def equality(root):
     print("Grammar equality")
-    return None
+    tokenList = root.value
+    for i in range(len(tokenList)):
+        if tokenList[i].value == '==' or tokenList[i].value == '!=':
+            relationN = Node(tokenList[:i])
+            equalityN =  Node(tokenList[i:])
+            root.children.append(Node(relationN))
+            root.children.append(Node(equalityN))
+            return relation(relationN) and equality1(equalityN)
+    relationN = Node(tokenList)
+    root.children.append(Node(relationN))
+    return relation(relationN)
 
 def equality1(root):
     print("Grammar equality1")
-    return None
+    tokenList = root.value
+    root.children.append(Node(tokenList[0]))
+    tokenList = tokenList[1:]
+    for i in range(len(tokenList)):
+        if tokenList[i].value == '==' or tokenList[i].value == '!=':
+            relationN = Node(tokenList[:i])
+            equalityN =  Node(tokenList[i:])
+            root.children.append(Node(relationN))
+            root.children.append(Node(equalityN))
+            return relation(relationN) and equality1(equalityN)
+    relationN = Node(tokenList)
+    root.children.append(Node(relationN))
+    return relation(relationN)
+
+def relation(root):
+    print("Grammar relation")
+    tokenList = root.value
+    for i in range(len(tokenList)):
+        if tokenList[i].value == '<' or tokenList[i].value == '>' or tokenList[i].value == '>=' or tokenList[i].value == '<=':
+            exprN = Node(tokenList[:i])
+            relationN =  Node(tokenList[i:])
+            root.children.append(Node(exprN))
+            root.children.append(Node(relationN))
+            return expr(exprN) and relation1(relationN)
+    exprN = Node(tokenList)
+    root.children.append(Node(exprN))
+    return expr(exprN)
+
+def relation1(root):
+    print("Grammar relation1")
+    tokenList = root.value
+    root.children.append(Node(tokenList[0]))
+    tokenList = tokenList[1:]
+    for i in range(len(tokenList)):
+        if tokenList[i].value in ['<', '>', '<=', '>=']:
+            exprN = Node(tokenList[:i])
+            relationN =  Node(tokenList[i:])
+            root.children.append(Node(exprN))
+            root.children.append(Node(relationN))
+            return expr(exprN) and relation1(relationN)
+    exprN = Node(tokenList)
+    root.children.append(Node(exprN))
+    return expr(exprN)
 
 def expr(root):
     print("Grammar expr")
-    return None
+    tokenList = root.value
+    for i in range(len(tokenList)):
+        if tokenList[i].value == '+' or tokenList[i].value == '-':
+            termN = Node(tokenList[:i])
+            exprN =  Node(tokenList[i:])
+            root.children.append(Node(termN))
+            root.children.append(Node(exprN))
+            return term(termN) and expr1(exprN)
+    termN = Node(tokenList)
+    root.children.append(Node(termN))
+    return term(termN)
 
 def expr1(root):
     print("Grammar expr1")
-    return None
+    tokenList = root.value
+    root.children.append(Node(tokenList[0]))
+    tokenList = tokenList[1:]
+    for i in range(len(tokenList)):
+        if tokenList[i].value == '+' or tokenList[i].value == '-':
+            termN = Node(tokenList[:i])
+            exprN =  Node(tokenList[i:])
+            root.children.append(Node(termN))
+            root.children.append(Node(exprN))
+            return term(termN) and expr1(exprN)
+    termN = Node(tokenList)
+    root.children.append(Node(termN))
+    return term(termN)
 
 def term(root):
     print("Grammar term")
-    return None
+    tokenList = root.value
+    for i in range(len(tokenList)):
+        if tokenList[i].value in ['*', '/', '%', '//']:
+            factorN = Node(tokenList[:i])
+            termN =  Node(tokenList[i:])
+            root.children.append(Node(factorN))
+            root.children.append(Node(termN))
+            return factor(factorN) and term1(termN)
+    factorN = Node(tokenList)
+    root.children.append(Node(factorN))
+    return factor(factorN)
 
 def term1(root):
     print("Grammar term1")
-    return None
-
-def oper(root):
-    print("Grammar term1")
-    return None
+    tokenList = root.value
+    root.children.append(Node(tokenList[0]))
+    tokenList = tokenList[1:]
+    for i in range(len(tokenList)):
+        if tokenList[i].value in ['*', '/', '%', '//']:
+            factorN = Node(tokenList[:i])
+            termN =  Node(tokenList[i:])
+            root.children.append(Node(factorN))
+            root.children.append(Node(termN))
+            return factor(factorN) and term1(termN)
+    factorN = Node(tokenList)
+    root.children.append(Node(factorN))
+    return factor(factorN)
 
 def factor(root):
-    print("Grammar term1")
-    return None
-
-def boolean(root):
-    print("Grammar term1")
-    return None
+    print("Grammar factor")
+    tokenList = root.value #len of token list should be 1
+    if len(tokenList) == 1: 
+        root.children.append(Node(tokenList[0]))
+        return True
+    else:
+        print("len of token list should be 1")
+        return ValueError
